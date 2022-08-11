b39e6c23adc880145cc8a7aed5f5f95877f5e478
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-11 17:02:09 +0300

    finish HW1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import MessageStyles from "./Message.module.scss"
 
-function Message() {
+interface Props {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: Props) {
     return (
-        <div>
+        <div className={MessageStyles.message}>
 
+            <img className={MessageStyles.userAvatar} src={props.avatar} alt="User Avatar"/>
+            <div className={MessageStyles.textBlock}>
+                <div className={MessageStyles.messagePointer}/>
+                <span className={MessageStyles.userName}>{props.name}</span>
+                <div className={MessageStyles.messageText}>{props.message}
+                    <span className={MessageStyles.messageTime}>{props.time}</span>
+                    <div className={MessageStyles.messageTimeMask}/>
+                </div>
+
+            </div>
         </div>
     )
 }
