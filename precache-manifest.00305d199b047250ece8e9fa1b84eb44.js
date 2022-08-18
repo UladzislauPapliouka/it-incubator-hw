f5efd53ab1cb35693b9a1665794f10d8ef92afbc
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-18 14:42:15 +0300

    Updates

---
+++ precache-manifest.00305d199b047250ece8e9fa1b84eb44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76763c4006bd8156ee85d487fef36ea",
+    "url": "/it-incubator-hw/index.html"
+  },
+  {
+    "revision": "58365403f0df59208120",
+    "url": "/it-incubator-hw/static/css/main.7627e0be.chunk.css"
+  },
+  {
+    "revision": "1730c0b7b9eb3c674382",
+    "url": "/it-incubator-hw/static/js/2.96fac44a.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/it-incubator-hw/static/js/2.96fac44a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58365403f0df59208120",
+    "url": "/it-incubator-hw/static/js/main.69695a3d.chunk.js"
+  },
+  {
+    "revision": "07eb19f91c8fd953834f",
+    "url": "/it-incubator-hw/static/js/runtime-main.011c1d83.js"
+  }
+]);
