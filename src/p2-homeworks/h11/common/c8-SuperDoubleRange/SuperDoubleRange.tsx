2dc55d6265f509866842df8d6e75d631e3885b3f
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-28 14:51:16 +0300

    finished HW11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
+import {Box, Slider} from "@mui/material";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
+    onChangeRange?: (value: [number, number] | number[]) => void
     value?: [number, number]
-    // min, max, step, disable, ...
 }
+
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
     }
 ) => {
+
     // сделать самому, можно подключать библиотеки
+    const [value_range, setValue] = React.useState<number[]>(value ? value : [0, 100]);
+
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+        onChangeHandler(newValue as number[])
+    };
+    const onChangeHandler = (arr: number[]) => {
+        onChangeRange && onChangeRange(arr)
+    }
 
     return (
-        <>
-            DoubleRange
-        </>
+        <Box sx={{width: 300}}>
+            <Slider
+                getAriaLabel={() => 'SuperDoubleRange'}
+                value={value ? value : value_range}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+            />
+        </Box>
     )
 }
 
