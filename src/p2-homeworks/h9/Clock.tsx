e48ddcdb322a8b560dbd63e4a9f2852aa5358a58
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-28 14:01:38 +0300

    finished HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,58 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
-
+    const monthes: { [key: number]: string } = {
+        0: "January",
+        1: "February",
+        2: "March",
+        3: "April",
+        4: "May",
+        5: "June",
+        6: "July",
+        7: "August",
+        8: "September",
+        9: "October",
+        10: "November",
+        11: "December",
+    }
+    const days: { [key: number]: string } = {
+        0: "sunday",
+        1: "monday",
+        2: "tuesday",
+        3: "wednesday",
+        4: "thursday",
+        5: "friday",
+        6: "saturday",
+    }
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = `${date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}
+                        :
+                        ${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}
+                        :
+                        ${date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()}
+    `
+    const stringDate = `${date.getFullYear()} ${monthes[date.getMonth()]} ${date.getDate()}`
 
     return (
         <div>
