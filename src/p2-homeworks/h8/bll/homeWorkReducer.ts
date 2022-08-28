314056fef896dee170e48f2db71fa70576db739a
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-28 13:42:08 +0300

    finished HW7 and HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,33 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: homeWorkActions): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return [...state.sort(
+                (user1: UserType, user2: UserType) => {
+                    if (user1.name > user2.name) {
+                        return action.payload === "up" ? 1 : -1;
+                    }
+                    if (user1.name < user2.name) {
+                        return action.payload === "up" ? -1 : 1;
+                    }
+                    return 0;
+                }
+            )]
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(user => user.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
+type homeWorkActionSort = {
+    type: "sort"
+    payload: "up" | "down"
+}
+type homeWorkActionCheck = {
+    type: "check"
+    payload: number
+}
+type homeWorkActions = homeWorkActionCheck | homeWorkActionSort
