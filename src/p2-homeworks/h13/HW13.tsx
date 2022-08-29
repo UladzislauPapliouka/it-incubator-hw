d92faaea10893bb5a816a676de5956292add0cb7
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-08-29 14:52:04 +0300

    finished HW12 and HW13

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../h4/common/c3-SuperCheckbox/SuperCheckbox";
+import {requestAPI} from "./api/RequestsAPI";
+
+export const Request = () => {
+
+    const [success, setSuccess] = useState(false)
+
+    const onChangeChecked = (value: boolean) => {
+        setSuccess(value)
+    }
+
+    const onClickHandler = () => {
+        requestAPI.post(success)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(reject => {
+                console.log({...reject});
+                console.log(reject.response ? reject.response.data.errorText : reject.message);
+            })
+
+    }
+
+    return (
+        <div>
+            <SuperCheckbox checked={success} onChangeChecked={onChangeChecked}/>
+            <SuperButton onClick={onClickHandler}>POST</SuperButton>
+        </div>
+    );
+};
+
+export default Request
